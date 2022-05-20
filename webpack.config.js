ecdec5e7033c032dfa77575a734796c0903f90fa
J.L.Chen <cjl255352@gmail.com>
2022-05-20 10:15:54 +0800

    1

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
   },
 };
