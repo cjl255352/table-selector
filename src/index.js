9e43565272e459bc71ba6535d05f4a6e9b011ac5
J.L.Chen <cjl255352@gmail.com>
2022-05-21 15:19:15 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import "./style.scss";
 
 const component = () => {
-  const a = [1, 2, 3];
   const set = new Set([1, 2, 3]);
-  [1, 2, 3].includes(2);
-  const b = () => {
-    return a;
-  };
-  console.log(...b);
+  console.log(set);
 };
 
 component();
