ff6dbc728370f858eaa074ea649bba6d16abf564
J.L.Chen <cjl255352@gmail.com>
2022-05-21 13:28:01 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
-import _ from "lodash";
 import "./style.scss";
-import Print from "./test";
 
 function component() {
   const element = document.createElement("div");
+  element.innerText = "hello word";
+  element.classList.add("hello");
 
-  // lodash 在当前 script 中使用 import 引入
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-     element.classList.add("hello");
-     console.log(1)
-  element.onclick = Print.bind(null, "Hello webpack!");
   return element;
 }
 
