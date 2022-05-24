ffef984043351d67bdd482fc00a02e1dc7e71121
J.L.Chen <cjl255352@gmail.com>
2022-05-24 21:45:51 +0800

    1

--- src/index.js
+++ src/index.js
@@ -2,10 +2,32 @@
 import { patch, h, jsx, initOptions } from "@/libs";
 import "@/style/index.scss";
 
+function destroy(vnode, delay = 0) {
+  setTimeout(() => {
+    patch(vnode, h("!"));
+  }, delay);
+}
+
+function initWrapper(options, status = "enter") {
+  let vnode = (
+    <div
+      class={{
+        [`${options.classPrefix}-wrapper`]: true,
+        [`${options.classPrefix}-wrapper-enter`]: status == "enter",
+        [`${options.classPrefix}-wrapper-leave`]: status == "leave",
+      }}
+      on={{
+        click: () => {
+          destroy(patch(vnode, initWrapper(options, "leave")), 200);
+        },
+      }}
+    />
+  );
+  return vnode;
+}
+
 let wrapper;
 let dialog;
-
-console.log(initOptions());
 
 function getMountPoint(count = 1) {
   const fragment = document.createDocumentFragment();
@@ -24,62 +46,56 @@ function close() {
   patch(dialog, h("!"));
 }
 
-function initWrapper() {
-  return (
-    <div class={{ "table-selector-wrapper": true }} on={{ click: close }} />
-  );
-}
+// let closeBtn = (isHover = false) => {
+//   return (
+//     <div
+//       class={{ "flow-select-close": true, "is-hover": isHover }}
+//       on={{
+//         click: close,
+//         mouseover: (e, vnode) => {
+//           patch(vnode, closeBtn(!isHover));
+//         },
+//         mouseleave: (e, vnode) => {
+//           patch(vnode, closeBtn(!isHover));
+//         },
+//       }}
+//     />
+//   );
+// };
 
-let closeBtn = (isHover = false) => {
-  return (
-    <div
-      class={{ "flow-select-close": true, "is-hover": isHover }}
-      on={{
-        click: close,
-        mouseover: (e, vnode) => {
-          patch(vnode, closeBtn(!isHover));
-        },
-        mouseleave: (e, vnode) => {
-          patch(vnode, closeBtn(!isHover));
-        },
-      }}
-    />
-  );
-};
-
-function initDialog() {
-  return (
-    <div class={{ "flow-select-dialog": true }}>
-      <div class={{ "flow-select-header": true }}>
-        <span class={{ "flow-select-title": true }}>流程选择</span>
-        {closeBtn()}
-      </div>
-      <div class={{ "flow-select-filter": true }}>
-        <input
-          class={{ "flow-select-keyword": true }}
-          attrs={{ placeholder: "请输入关键字" }}
-        />
-        <div
-          class={{ "flow-select-search": true }}
-          on={{
-            click: (e) => {
-              console.log(e);
-            },
-          }}
-        />
-      </div>
-    </div>
-  );
-}
+// function initDialog() {
+//   return (
+//     <div class={{ "flow-select-dialog": true }}>
+//       <div class={{ "flow-select-header": true }}>
+//         <span class={{ "flow-select-title": true }}>流程选择</span>
+//         {closeBtn()}
+//       </div>
+//       <div class={{ "flow-select-filter": true }}>
+//         <input
+//           class={{ "flow-select-keyword": true }}
+//           attrs={{ placeholder: "请输入关键字" }}
+//         />
+//         <div
+//           class={{ "flow-select-search": true }}
+//           on={{
+//             click: (e) => {
+//               console.log(e);
+//             },
+//           }}
+//         />
+//       </div>
+//     </div>
+//   );
+// }
 
 function open(options = {}) {
   options = initOptions(options);
   console.log(options);
-  const [wrapperPoint, dialogPoint] = getMountPoint(2);
-  wrapper = initWrapper();
-  dialog = initDialog();
+  const wrapperPoint = getMountPoint();
+  wrapper = initWrapper(options);
+  // dialog = initDialog();
   patch(wrapperPoint, wrapper);
-  patch(dialogPoint, dialog);
+  // patch(dialogPoint, dialog);
 }
 
 export { open, close };
