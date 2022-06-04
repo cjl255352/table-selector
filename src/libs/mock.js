3369729ec3f11696e808b224aa8c5a4e2f416d5a
J.L.Chen <cjl255352@gmail.com>
2022-06-04 18:54:38 +0800

    Update options.js

--- src/libs/mock.js
+++ src/libs/mock.js
@@ -1,45 +1,89 @@
+let index = 0;
+
 function mock(params, done) {
-  console.log(`搜索关键字：`, params);
+  index++;
   setTimeout(() => {
-    done({
-      rows: [
-        {
-          $index: 1,
-          id: 1,
-          domain: "非洲业务部",
-          name: "流程名称xxx非洲业务部",
-          owner: "陈加隆",
-        },
-        {
-          $index: 2,
-          id: 2,
-          domain: "澳洲业务部",
-          name: "流程名称xxx澳洲业务部",
-          owner: "陈加隆",
-        },
-        {
-          $index: 3,
-          id: 3,
-          domain: "美洲业务部",
-          name: "流程名称xxx美洲业务部",
-          owner: "陈加隆",
-        },
-        {
-          $index: 4,
-          id: 4,
-          domain: "亚洲业务部",
-          name: "流程名称xxx亚洲业务部",
-          owner: "陈加隆",
-        },
-        {
-          $index: 5,
-          id: 5,
-          domain: "欧洲业务部",
-          name: "流程名称xxx欧洲业务部",
-          owner: "陈加隆",
-        },
-      ],
-    });
+    if (index % 2) {
+      done({
+        rows: [
+          {
+            $index: 1,
+            id: 1,
+            domain: "非洲业务部",
+            name: "流程名称xxx非洲业务部",
+            owner: "陈加隆",
+          },
+          {
+            $index: 2,
+            id: 2,
+            domain: "澳洲业务部",
+            name: "流程名称xxx澳洲业务部",
+            owner: "陈加隆",
+          },
+          {
+            $index: 3,
+            id: 3,
+            domain: "美洲业务部",
+            name: "流程名称xxx美洲业务部",
+            owner: "陈加隆",
+          },
+          {
+            $index: 4,
+            id: 4,
+            domain: "亚洲业务部",
+            name: "流程名称xxx亚洲业务部",
+            owner: "陈加隆",
+          },
+          {
+            $index: 5,
+            id: 5,
+            domain: "欧洲业务部",
+            name: "流程名称xxx欧洲业务部",
+            owner: "陈加隆",
+          },
+        ],
+      });
+    } else {
+      done({
+        rows: [
+          {
+            $index: 6,
+            id: 6,
+            domain: "非洲业务部",
+            name: "流程名称xxx非洲业务部",
+            owner: "陈加隆",
+          },
+          {
+            $index: 7,
+            id: 7,
+            domain: "澳洲业务部",
+            name: "流程名称xxx澳洲业务部",
+            owner: "陈加隆",
+          },
+          {
+            $index: 8,
+            id: 8,
+            domain: "美洲业务部",
+            name: "流程名称xxx美洲业务部",
+            owner: "陈加隆",
+          },
+          {
+            $index: 9,
+            id: 9,
+            domain: "亚洲业务部",
+            name: "流程名称xxx亚洲业务部",
+            owner: "陈加隆",
+          },
+          {
+            $index: 10,
+            id: 10,
+            domain: "欧洲业务部",
+            name: "流程名称xxx欧洲业务部",
+            owner: "陈加隆",
+          },
+        ],
+      });
+    }
   }, 1000);
 }
 
